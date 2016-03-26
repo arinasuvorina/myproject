772febc4ebebe0eb9db5d281f8bc59da79722b46
Арина <arsomuko@yandex.ru>
2016-03-26 16:32:18 +0500

    Fix server

--- server.py
+++ server.py
@@ -6,16 +6,17 @@ sock.listen(10)
  
 while True: 
     conn, addr = sock.accept() 
-    data = conn.recv(1024) 
+    data = conn.recv(2048) 
     dataAsStr = data.decode()
     dataFirstStr = dataAsStr.split('\n')[0]
     path = dataFirstStr.split(' ')[1]
     path = path[1:] 
     if path == "": 
-        path = "index.html" 
-    file = open(path, 'rb') 
-    conn.send(file.read()) 
-    file.close() 
+        path = "index.html"
+    if (path != "favicon.ico"):
+        file = open(path, 'rb') 
+        conn.send(file.read()) 
+        file.close() 
     conn.close()
     
 sock.close() 
